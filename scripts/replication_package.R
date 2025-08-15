e0c21b20958009bddf24decba05e948199b35804
Julia Rohrer <juliarohrer@MacBook-Air-von-Julia.local>
2025-08-15 11:44:14 +0200

    Did some planning for the replication package

---
+++ scripts/replication_package.R
@@ -0,0 +1,17 @@
+# Generate replication package
+
+# Wait until all analyses are set in stone
+
+# library(knitr)
+# purl("notebooks/relationship_friends.qmd", output = "scripts/worked_example_1.R")
+# purl("notebooks/deskmates.qmd", output = "scripts/worked_example_2.R")
+
+# Note: remove references to here() manually
+# Add the loading to the front of the script 
+# so there is no need for a separate load script in the replication package
+
+
+# bundle up: 
+# data folder with the data for examples
+# worked_example scripts
+# README.txt
